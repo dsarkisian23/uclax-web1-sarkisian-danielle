a8de9b56b087e63c2811483d5b933296ef805d0a
danielle sarkisian <dsarkisian23@ucla.edu>
2024-01-29 18:55:12 -0800

    L3: Complete

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import Inset from "@/Common/PagesLayout/Inset";
+const Home = () => {
+    return (
+        <HomeStyled>
+            <Inset>
+                <h1>Home</h1>
+            </Inset>
+        </HomeStyled>
+    );
+};
+
+export default Home;
+
+const HomeStyled = styled.div``;
---
+++ src/Pages/Staff.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+import Inset from "@/Common/PagesLayout/Inset";
+const Staff = () => {
+    return (
+        <StaffStyled>
+            <Inset>
+                <h1>Staff</h1>
+            </Inset>
+        </StaffStyled>
+    );
+};
+
+export default Staff;
+
+const StaffStyled = styled.div``;
