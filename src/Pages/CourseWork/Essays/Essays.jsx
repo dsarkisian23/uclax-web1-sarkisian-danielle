d12f89cb565815ce35133f6eb5d96f158f293500
danielle sarkisian <dsarkisian23@ucla.edu>
2024-02-12 19:12:47 -0800

    Essay Answers

--- src/Pages/CourseWork/Essays/Essays.jsx
+++ src/Pages/CourseWork/Essays/Essays.jsx
@@ -6,14 +6,44 @@ const Essays = () => {
     return (
         <EssaysStyled>
             <h2>Essays</h2>
-            <SingleEssay question={`Q1: Sample question...`}>
-                Answer 1 goes here.
+            <SingleEssay
+                question={`Explain the difference between HTML, CSS and JavaScript.`}
+            >
+                <ul>
+                    HTML is like the skeleton of the webpage, outlining where
+                    everything goes and how the webpage is structured. CSS is
+                    the decoration on the webpage, adding stylistic features
+                    such as color, font size, and more. JavaScript allows the
+                    webpage to be interactive, coding specific responses to
+                    certain actions that the webpage visitor takes. For example,
+                    you could use JavaScript to code that if you hover over a
+                    button, the button changes color.
+                </ul>
             </SingleEssay>
-            <SingleEssay question={`Q2: Sample question...`}>
-                Answer 2 goes here.
+            <SingleEssay
+                question={`What is the difference between Git and Github?`}
+            >
+                <ul>
+                    <li>
+                        <b>Git</b> = tracking service allowing multiple
+                        programmers to track changes that are made in a code
+                    </li>
+                    <li>
+                        <b>GitHub</b> = online platform that stores repositories
+                        of git
+                    </li>
+                </ul>
             </SingleEssay>
-            <SingleEssay question={`Q3: Sample question...`}>
-                Answer 3 goes here.
+            <SingleEssay
+                question={`What is the difference between JQuery and React?`}
+            >
+                <ul>
+                    Both are libraries of code, but JQuery uses html and
+                    simplified DOM whereas React uses a virtual DOM. React is
+                    great for UI, whereas JQuery is great for simplifying
+                    complex code. React was originally founded by Facebook
+                    developers.
+                </ul>
             </SingleEssay>
         </EssaysStyled>
     );
